d7b2689bdf545b02714af0369fb53a2a6ea8cf97
jacobvetere <jacobvetere@gmail.com>
2022-07-06 06:02:25 -0400

    ADDED Tokenization process and process to assign meaning to said tokens. Process to generate Abstract Syntax Trees (Action Trees) Basic Library function of LET Memory storage module and related data structures - String and Array Memory Mod Let testing in UtilizeTest Begin tokenizer rewrite towards full use of Regular Expression need to to everything other than fun and write text for fun
    TODO
    Refactor Action Tree to be cleaner: Low
    Continue Implementing Memory Maps: High
    Move all type alias's to separate file: Low
    Add Type relationships to Action Trees: High
    Finish Let: Very High
    Begin the rest of the utilize functions: High
    Build data structure packages: Medium
    
    Statement of Philosophy
    Ly is a Functional, Strongly typed, Interpreted, JVM Based Programing. [To be exapanded]

--- src/main/kotlin/lexer/RegexTokenizer.kt
+++ src/main/kotlin/lexer/RegexTokenizer.kt
@@ -1,16 +1,29 @@
 package lexer
 
-import java.util.regex.Matcher
-
-val FUN_PATTERN: Regex = Regex(
+val FUNCTION_ASSIGNMENT: Regex = Regex(
+    "(?<Assign>fun)\\s" +
+            "(?<Function>[a-z A-Z]?[a-z A-Z 0-9]+)" +
+            "(?<Variables>\\(.+\\))\\s?" +
+            "(?<Return>->[a-z A-Z]+)?(\\{)"
+)
+val FUNCTION_CALL: Regex = Regex(
+    "(?<Function>[a-z A-Z]?[a-z A-Z 0-9]+)" +
+            "(?<Variable>\\(.+\\))"
+)
+val VARIABLE_ASSIGNMENT: Regex = Regex(
     "(?<Assign>let)" +
             "(?<Alias>[a-z A-Z]?[a-z A-Z 0-9]+)" +
             "(?<Type>:\\s[a-z A-Z]?[a-z A-Z 0-9]+)" +
             "(?<Assignment>=)" +
             "(?<Variable>\".+\"|.+)")
+val VARIABLE_REASSIGNMENT: Regex = Regex(
+    "(?<Alias>[a-z A-Z]?[a-z A-Z 0-9]+)" +
+            "(?<Assignment>=)" +
+            "(?<Variable>\".+\"|.+)"
+)
 
-val pullFunctionsFromLine: (String) -> MatchResult = { line: String ->
-    val matches: MatchResult = FUN_PATTERN.find(line)!!
+val PULL_VARIABLE_ASSIGNIMENT: (String) -> MatchResult = { line: String ->
+    val matches: MatchResult = VARIABLE_ASSIGNMENT.find(line)!!
     println(matches.groups)
     matches
 }
